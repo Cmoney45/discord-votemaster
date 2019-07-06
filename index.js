ad6c74efdfe0da0b61a860a1910487a289b717ad
Cmoney <cascherer42@gmail.com>
2019-07-06 15:53:21 -0500

    Initial commit and modularize functions

--- index.js
+++ index.js
@@ -1,151 +1,152 @@
 const private = require('./private.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Poll = require("./controller/Poll");
 
 const defaults = {
-	timeout: 30,
+	timeout: 1,
 	color: 2555834,
 	triggers: {newPoll: '!newpoll', vote: '!vote', results: '!results'},
 	appName: 'Votemaster'
 };
-var pollIndex = 0, polls = new Map();
+// var pollIndex = 0, polls = new Map();
 
-// The corresponding emojis are used as unique keys for choices within each poll object
-const emoji = {
-	numbers: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
-		.map((value, index) => [String(index), `:${value}:`]),
-	letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-		.map(value => [value, `:regional_indicator_${value}:`]),
-	yn: [['yes','**Yes**'],['no','**No**']],
-	maybe: ['maybe','**Maybe**']
-};
+// // The corresponding emojis are used as unique keys for choices within each poll object
+// const emoji = {
+// 	numbers: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+// 		.map((value, index) => [String(index), `:${value}:`]),
+// 	letters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+// 		.map(value => [value, `:regional_indicator_${value}:`]),
+// 	yn: [['yes','**Yes**'],['no','**No**']],
+// 	maybe: ['maybe','**Maybe**']
+// };
 
-class Poll {
-	constructor(opt) {
-		var args = opt.arguments;
-		this.name = opt.name;
-		this.id = pollIndex;
-			pollIndex++;
+// class Poll {
+// 	constructor(opt) {
+// 		var args = opt.arguments;
+// 		this.name = opt.name;
+// 		this.id = pollIndex;
+// 			pollIndex++;
 
-		this.choices = new Map();
-		opt.choices.forEach((value, index) => {
-			this.choices.set(emoji[opt.emojiType][index][0], {
-				name: value,
-				emoji: emoji[opt.emojiType][index][1],
-				votes: 0
-			});
-		});
-		if(args.maybe || args.idk) {
-			this.choices.set(emoji.maybe[0], {
-				name: 'I don\'t know.',
-				emoji: emoji.maybe[1],
-				votes: 0
-			});
-		}
+// 		this.choices = new Map();
+// 		opt.choices.forEach((value, index) => {
+// 			this.choices.set(emoji[opt.emojiType][index][0], {
+// 				name: value,
+// 				emoji: emoji[opt.emojiType][index][1],
+// 				votes: 0
+// 			});
+// 		});
+// 		if(args.maybe || args.idk) {
+// 			this.choices.set(emoji.maybe[0], {
+// 				name: 'I don\'t know.',
+// 				emoji: emoji.maybe[1],
+// 				votes: 0
+// 			});
+// 		}
 
-		this.disallowEdits = args.lock || false;
-		this.blind = args.blind || false;
-		this.reactionVoting = args.reactions || args.rxn || false;
-		this.allowMultipleVotes = this.reactionVoting || args.mult || args.multiple || false;
-		this.restrictRole = args.role || false;
-		this.dontCloseEarly = args.lo || args.leaveopen || args.dontcloseearly || false;
-		this.timeout = opt.timeout || 30;
-		this.color = opt.color;
+// 		this.disallowEdits = args.lock || false;
+// 		this.blind = args.blind || false;
+// 		this.reactionVoting = args.reactions || args.rxn || false;
+// 		this.allowMultipleVotes = this.reactionVoting || args.mult || args.multiple || false;
+// 		this.restrictRole = args.role || false;
+// 		this.dontCloseEarly = args.lo || args.leaveopen || args.dontcloseearly || false;
+// 		this.timeout = opt.timeout || 30;
+// 		this.color = opt.color;
 
-		this.footNote = opt.notes || ' ';
-		this.footNote += `${opt.notes ? '| ' : ''}This is Poll #${this.id}. It will expire in ${this.timeout} minutes.`;
+// 		this.footNote = opt.notes || ' ';
+// 		this.footNote += `${opt.notes ? '| ' : ''}This is Poll #${this.id}. It will expire in ${this.timeout} minutes.`;
 
-		this.open = false;
-		this.totalVotes = 0;
+// 		this.open = false;
+// 		this.totalVotes = 0;
 
-		this.voters = new Map();
+// 		this.voters = new Map();
 
-		this.server = opt.server;
+// 		this.server = opt.server;
 
-		this.timeCreated = new Date();
-	}
+// 		this.timeCreated = new Date();
+// 	}
 
-	// Function to initiate timer
-	startTimer() {
-		this.open = true;
-		setTimeout(function() {
-			this.open = false;
-		}.bind(this), this.timeout * 60 * 1000);
-	}
+// 	// Function to initiate timer
+// 	startTimer() {
+// 		this.open = true;
+// 		setTimeout(function() {
+// 			this.open = false;
+// 		}.bind(this), this.timeout * 60 * 1000);
+// 	}
 
-	// Log votes (if the poll is open and unlocked/user hasn't voted)
-	vote(key, user) {
-		console.log(key, this.choices);
-		if(this.open) {
-			if(this.lock && this.voters.get(user.id)) {
-				return {
-					success: false,
-					reason: 'lock',
-					message: "Sorry, this is a locked poll (you can't edit your vote) and you've already voted."
-				};
-			} else if(!this.choices.get(key)) {
-				return {
-					success: false,
-					reason: 'invalid',
-					message: "That option is not a valid choice, so I can't log your vote. Try sending just the letter, number, or word that corresponds with the choice."
-				};
-			} else if(this.voters.get(user.id)) {
-				// User has already voted, we need to change their vote
-				let oldVoter = this.voters.get(user.id);
-				this.choices.get(oldVoter.vote.choice).votes--;
+// 	// Log votes (if the poll is open and unlocked/user hasn't voted)
+// 	vote(key, user) {
+// 		console.log(key, this.choices);
+// 		if(this.open) {
+// 			if(this.lock && this.voters.get(user.id)) {
+// 				return {
+// 					success: false,
+// 					reason: 'lock',
+// 					message: "Sorry, this is a locked poll (you can't edit your vote) and you've already voted."
+// 				};
+// 			} else if(!this.choices.get(key)) {
+// 				return {
+// 					success: false,
+// 					reason: 'invalid',
+// 					message: "That option is not a valid choice, so I can't log your vote. Try sending just the letter, number, or word that corresponds with the choice."
+// 				};
+// 			} else if(this.voters.get(user.id)) {
+// 				// User has already voted, we need to change their vote
+// 				let oldVoter = this.voters.get(user.id);
+// 				this.choices.get(oldVoter.vote.choice).votes--;
 				
-				this.choices.get(key).votes++;
-				this.voters.set(user.id, {
-					user: user,
-					vote: {
-						time: new Date(),
-						choice: key
-					}
-				});
-				return {
-					success: true,
-					reason: '',
-					message: `Great, I changed your vote to "${this.choices.get(key).name}"!`
-				};
+// 				this.choices.get(key).votes++;
+// 				this.voters.set(user.id, {
+// 					user: user,
+// 					vote: {
+// 						time: new Date(),
+// 						choice: key
+// 					}
+// 				});
+// 				return {
+// 					success: true,
+// 					reason: '',
+// 					message: `Great, I changed your vote to "${this.choices.get(key).name}"!`
+// 				};
 
-			} else {
-				this.choices.get(key).votes++;
-				// While we technically *could* use the user object as the key, that would be difficult to access. id should be unique.
-				this.voters.set(user.id, {
-					user: user,
-					vote: {
-						time: new Date(),
-						choice: key
-					}
-				});
-				return {
-					success: true,
-					reason: '',
-					message: `Great, I logged your vote for "${this.choices.get(key).name}"!`
-				};
-			}
-		} else {
-			return {
-				sucess: false,
-				reason: 'timeout',
-				message: "Sorry, this poll has timed out and can no longer be voted on."
-			};
-		}
-	}
+// 			} else {
+// 				this.choices.get(key).votes++;
+// 				// While we technically *could* use the user object as the key, that would be difficult to access. id should be unique.
+// 				this.voters.set(user.id, {
+// 					user: user,
+// 					vote: {
+// 						time: new Date(),
+// 						choice: key
+// 					}
+// 				});
+// 				return {
+// 					success: true,
+// 					reason: '',
+// 					message: `Great, I logged your vote for "${this.choices.get(key).name}"!`
+// 				};
+// 			}
+// 		} else {
+// 			return {
+// 				sucess: false,
+// 				reason: 'timeout',
+// 				message: "Sorry, this poll has timed out and can no longer be voted on."
+// 			};
+// 		}
+// 	}
 
-	close() {
-		// Calling close() on a closed poll has no effect
-		if(this.open) {
-			this.open = false;
-			return true;
-		} else return false;
-	}
+// 	close() {
+// 		// Calling close() on a closed poll has no effect
+// 		if(this.open) {
+// 			this.open = false;
+// 			return true;
+// 		} else return false;
+// 	}
 
-	get chart() {
-		// TODO generate charts of results
-		return null;
-	}
-}
+// 	get chart() {
+// 		// TODO generate charts of results
+// 		return null;
+// 	}
+// }
 
 function generateDiscordEmbed(poll, type) {
 	var embed = {}, choiceList = ``, resultsList = ``;
--- private_example.js
+++
@@ -1,3 +0,0 @@
-// Get a token from https://discordapp.com/developers/applications/me
-// Replace YOUR TOKEN HERE with it and save as "private.js"
-module.exports.token = "YOUR TOKEN HERE";
